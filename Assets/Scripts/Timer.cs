1d6eeafc9346d58ff8d2892bd49c5ddd97a62ed3
sam1raa <sjost111@gmx.de>
2025-05-14 21:14:38 +0200

    Timer, menu

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] GameObject timerCanvas;
+    [SerializeField] GameObject scoreCanvas;
+    [SerializeField] TMP_Text scoreText;
+
+    [SerializeField] Image timerImage;
+
+    float timeElapsed = 0f;
+    [SerializeField] float timeTotal = 120f;
+
+    int score = 20;
+
+    void Update()
+    {
+        if(timeElapsed < timeTotal)
+        {
+            timeElapsed += Time.deltaTime;
+            timerImage.fillAmount = 1 - timeElapsed/timeTotal;
+        }
+        else
+        {
+            timerCanvas.SetActive(false);
+            scoreText.text = "Score: " + score;
+            scoreCanvas.SetActive(true);
+        }
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
