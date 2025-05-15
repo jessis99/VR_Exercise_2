4c37ff05d7083d18ea29715bdbf0f8ef6dc2745d
jessis99 <jessi@schifferweb.de>
2025-05-15 19:20:50 +0200

    points begin

---
+++ Assets/Scripts/Points.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Points : MonoBehaviour
+{
+    private int score;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
